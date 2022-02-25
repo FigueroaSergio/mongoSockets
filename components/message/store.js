2948c3409e314e4e67d46a671098c123a0a5d1ef
sergio figueroa <figueroa.sergio.26@gmail.com>
2022-02-25 08:35:45 +0100

    connet to mongo db

--- components/message/store.js
+++ components/message/store.js
@@ -1,9 +1,14 @@
-let list =[]
-function getMessages(){
-    return list
+const db = require("mongoose")
+const Model = require ("./model")
+db.connect(process.env.MONGOURL,{useNewUrlParser:true})
+console.log("[db]Connect successfully");
+
+async function getMessages(){
+    return await Model.find()
 }
 function addMessage(message){
-    list.push(message)
+    const myMessage= new Model(message);
+    myMessage.save()
 }
 
 module.exports={
